51b5445c4cc894b6c3f5561992425fcd630d3e17
Bihan-Banerjee <bihanbanerjee26@gmail.com>
2026-02-16 02:11:57 +0530

    build bugs fixed

---
+++ src/server/global.d.ts
@@ -0,0 +1,9 @@
+declare module "wav-encoder" {
+  const WavEncoder: any;
+  export default WavEncoder;
+}
+
+declare module "wav-decoder" {
+  const WavDecoder: any;
+  export default WavDecoder;
+}
